95ac8216794941614ca381562df9e53349dd929a
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-07-16 17:36:43 +0300

    init

---
+++ src/main/java/by/vasyabylba/crud/onetomany/CrudOneToManyApplication.java
@@ -0,0 +1,12 @@
+package by.vasyabylba.crud.onetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOneToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudOneToManyApplication.class, args);
+    }
+}
---
+++ src/test/java/by/vasyabylba/crud/onetomany/CrudOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vasyabylba.crud.onetomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
